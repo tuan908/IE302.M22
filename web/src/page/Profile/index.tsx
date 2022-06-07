0a8215ae61476bc15667a1e1b051892b4b60f438
UIT.tuanna <tuan.a1.bt@gmail.com>
2022-06-07 22:16:36 +0700

    Download image (30)

--- web/src/page/Profile/index.tsx
+++ web/src/page/Profile/index.tsx
@@ -1,37 +1,67 @@
+import { Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getUserProfile } from 'src/service/user.service';
-import { PostDetail } from 'src/ui/Post';
 
-interface UserInfoProps {
-  avatarUrl?: string;
-  username?: string;
-  email?: string;
+interface User {
   userId: string;
 }
 
+type ImageApi = {
+  comments: [];
+  createdTime: Date;
+  imageId: string;
+  updatedTime: Date;
+  content: string;
+
+  postStatus: string;
+  postReactCount?: number;
+  postId?: string;
+  postTitle: string;
+};
+
+type UserInfo = {
+  avatarUrl?: string;
+  email?: string;
+  list: Array<ImageApi>;
+  user: string;
+  userId: string;
+};
+
 function PinterestProfile() {
   const location = useLocation();
-  console.log(location.state);
-  const { avatarUrl, email, username, userId } =
-    location.state as UserInfoProps;
 
-  const [userInfo, setUserInfo] = useState<PostDetail>();
+  const { userId } = location.state as User;
+
+  const [userInfo, setUserInfo] = useState<UserInfo>();
 
   async function getUserInformation() {
     const rawData = await getUserProfile(userId);
+    console.log(rawData.data);
     setUserInfo(rawData.data);
   }
 
   useEffect(() => {
     getUserInformation();
   }, []);
-  console.log(userInfo);
+  console.log(userInfo?.list);
+
   return (
     <>
-      <img src={avatarUrl} alt="" />
-      <h4>{email}</h4>
-      <h4>{username}</h4>
+      <Typography variant="h5" gutterBottom>
+        Posts
+      </Typography>
+      <h4>{userInfo?.user}</h4>
+
+      {userInfo!?.list.map((item, index) => {
+        console.log(item);
+        return (
+          <div key={index}>
+            <Typography>{item!?.postReactCount}</Typography>
+            <Typography>{item!?.createdTime.toString()}</Typography>
+          </div>
+        );
+      })}
     </>
   );
 }
--- web/src/ui/Pin/index.tsx
+++ web/src/ui/Pin/index.tsx
@@ -1,6 +1,10 @@
-import { FC } from 'react';
+import { Download } from '@mui/icons-material';
+import { Tooltip } from '@mui/material';
+import axios from 'axios';
+import { FC, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PixabayPhoto } from 'src/api';
+import { v4 } from 'uuid';
 import { PinWrapper } from './Component';
 
 interface Props extends PixabayPhoto {}
@@ -8,24 +12,65 @@ interface Props extends PixabayPhoto {}
 const Pin: FC<Props> = ({ id, webformatURL, ...otherImageProps }: Props) => {
   const navigate = useNavigate();
 
+  const [isDisplay, setDisplay] = useState(false);
+
   const pinState = {
     id,
     webformatURL,
     ...otherImageProps,
   };
 
+  function onMouseEnter() {
+    setDisplay(true);
+  }
+
+  function onMouseLeave() {
+    setDisplay(false);
+  }
+
+  function onDownload() {
+    const imageName = v4();
+
+    axios
+      .get(webformatURL, {
+        responseType: 'arraybuffer',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/pdf',
+        },
+      })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const linkDownload = document.createElement('a');
+
+        linkDownload.setAttribute('download', `${imageName}.png`);
+        linkDownload.href = url;
+        document.body.appendChild(linkDownload);
+        linkDownload.click();
+      });
+  }
+
   return (
     <PinWrapper
-      onClick={() =>
-        navigate(`/image/${id}`, {
-          state: pinState,
-        })
-      }
+      onMouseEnter={() => onMouseEnter()}
+      onMouseLeave={() => onMouseLeave()}
+      style={{ position: 'relative' }}
     >
+      <Tooltip title="Download">
+        <Download
+          style={{ display: isDisplay ? '' : 'none', position: 'absolute' }}
+          onClick={() => onDownload()}
+        />
+      </Tooltip>
       <img
         src={webformatURL}
         style={{ display: 'block', width: '100%' }}
         alt=""
+        onClick={() =>
+          navigate(`/image/${id}`, {
+            state: pinState,
+          })
+        }
       />
     </PinWrapper>
   );
