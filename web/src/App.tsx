3dedc1e0eabd8a64bb6ff7f81b5c2b5ff9ec41bb
tuanna <tuan.a1.bt@gmail.com>
2022-06-15 00:26:25 +0700

    tuanna add login validation frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,41 @@
 import { CircularProgress } from '@mui/material';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Admin from 'src/page/Admin'
+import Admin from 'src/page/Admin';
+import { usePinterestSelector } from './redux/hooks';
 
 import privatePages from './util/page';
 
 function App() {
+  const user = usePinterestSelector((state) => state.userReducer.user);
   const Login = lazy(() => import('./page/Login'));
   const PinterestNotFound = lazy(() => import('./page/Login'));
   const Register = lazy(() => import('./page/Register'));
-
+  const [isLogin, setLogin] = useState(false);
+  useEffect(() => {
+    const userInfo = localStorage.getItem('user_info');
+    if (userInfo && userInfo !== 'undefined' && user !== {}) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }, []);
   return (
     <Suspense fallback={<CircularProgress />}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path='/admin' element={<Admin/>} />
+        <Route
+          path="/"
+          element={
+            isLogin ? (
+              <Navigate to="/home" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+        <Route path="/admin" element={<Admin />} />
 
         {privatePages.map((page, index) => (
           <Route
